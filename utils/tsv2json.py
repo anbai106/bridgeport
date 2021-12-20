ed5bbf38bd83f98535751ececaeb2b60cc292f04
anbai106 <anbai106@hotmail.com>
2021-12-20 09:07:46 -0500

    run ldsc for all pop

--- utils/tsv2json.py
+++ utils/tsv2json.py
@@ -2,6 +2,6 @@ import pandas as pd
 import glob
 import os
 
-os.chdir("/Users/tim/repos/bridgeport/data")
+os.chdir("/home/hao/Project/bridgeport/data")
 df = pd.read_csv("Significnat_after_correction_thres_5e-8.tsv", sep="\t")
 df.to_json("GWAS.json", orient="records")
