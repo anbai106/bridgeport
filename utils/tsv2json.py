275bddb0150507f707a4dd102366db9e64a3d68b
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2022-01-20 01:25:34 -0500

    some numbers are very small...

--- utils/tsv2json.py
+++ utils/tsv2json.py
@@ -1,24 +1,56 @@
 import pandas as pd
 import os
+import json
+
+# can't use pd.read_csv / to_json because it doesn't support arbitrary floating point precision
+def read_csv(csv_file):
+    with open(csv_file, 'r') as f:
+        lines = f.readlines()
+        header = lines[0].strip().split('\t')
+        data = []
+        for line in lines[1:]:
+            line = line.strip().split('\t')
+            row = {}
+            for i, col in enumerate(line):
+                row[header[i]] = col
+            data.append(row)
+    return data
+
+def write_json(json_file, data):
+    with open(json_file, 'w') as f:
+        f.write('[\n')
+        i = len(data)
+        for row in data:
+            i -= 1
+            json_row = []
+            j = len(row)
+            for key, value in row.items():
+                j -= 1
+                quoted_value = value if is_float(value) else '"' + value + '"'
+                json_row.append('"{}": {}{}'.format(key, quoted_value, ',' if j > 0 else ""))
+            last_comma = '' if i == 0 else ','
+            f.write('{'+' '.join(json_row) + '}'+last_comma+'\n')
+        f.write(']\n')
+
+def is_float(element):
+    try:
+        float(element)
+        return True
+    except ValueError:
+        return False
 
 os.chdir("/Users/tim/repos/bridgeport/data")
-df = pd.read_csv("Significnat_after_correction_thres_5e-8.tsv", sep="\t")
-df = df[df.P > 0]
-df.sort_values(by=["P"], inplace=True)
-df.to_json("GWAS.json", orient="records", double_precision=15)
+df = read_csv("Significnat_after_correction_thres_5e-8.tsv")
+write_json("GWAS.json", df)
 
-df = pd.read_csv("Gene_analysis/bridgeport_significant_gene.tsv", sep="\t")
-df.sort_values(by=["P"], inplace=True)
-df.to_json("gene_analysis.json", orient="records", double_precision=15)
+df = read_csv("Gene_analysis/bridgeport_significant_gene.tsv")
+write_json("gene_analysis.json", df)
 
-df = pd.read_csv("Genetic_correlation/bridgeport_genetic_correlation.tsv", sep="\t")
-df.sort_values(by=["P"], inplace=True)
-df.to_json("genetic_correlation.json", orient="records", double_precision=15)
+df = read_csv("Genetic_correlation/bridgeport_genetic_correlation.tsv")
+write_json("genetic_correlation.json", df)
 
-df = pd.read_csv("Heritability_estimate/bridgeport_heritability_estimate.tsv", sep="\t")
-df.sort_values(by=["Heritability"], inplace=True)
-df.to_json("heritability_estimate.json", orient="records", double_precision=15)
+df = read_csv("Heritability_estimate/bridgeport_heritability_estimate.tsv")
+write_json("heritability_estimate.json", df)
 
-df = pd.read_csv("IWAS/bridgeport_iwas.tsv", sep="\t")
-df.sort_values(by=["Pvalue"], inplace=True)
-df.to_json("IWAS.json", orient="records", double_precision=15)
+df = read_csv("IWAS/bridgeport_iwas.tsv")
+write_json("IWAS.json", df)
