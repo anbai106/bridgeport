a6ed1ce4c443892d8a7111451ecabe9872fe3ab1
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2022-01-29 12:14:59 -0600

    updated theme to match penn colors

--- src/App.js
+++ src/App.js
@@ -134,7 +134,7 @@ function App() {
   const renderAtlas = (c, cb = null) => {
     vtkContainerRef.current.innerHTML = '';
     progressRef.current.classList.remove('hidden');
-    progressRef.current.classList.add("progress", "progress-primary", "z-50", "col-span-12", "w-full");
+    progressRef.current.classList.add("progress", "progress-secondary", "z-50", "col-span-12", "w-full");
 
     // ----------------------------------------------------------------------------
     // Standard rendering code setup
@@ -212,7 +212,7 @@ function App() {
           progressRef.current.value = 0;
           progressRef.current.dataset.value = 0;
           progressRef.current.classList.add('hidden');
-          progressRef.current.classList.remove("progress", "progress-primary", "z-50", "col-span-12", "w-full");
+          progressRef.current.classList.remove("progress", "progress-secondary", "z-50", "col-span-12", "w-full");
           genericRenderer.resize(); // necessary! to fix blurriness
         }
       }
@@ -279,6 +279,7 @@ function App() {
       setSearched(true);
       searchBoxRef.current.value = event.state.searchQuery;
       submitSearch(searchBoxRef.current, false);
+      // eslint-disable-next-line eqeqeq
       if (event.state.atlas !== undefined && atlas != event.state.atlas) {
         animateIn(event.state.atlas)
       }
@@ -466,7 +467,7 @@ function App() {
     for (let i = 0; i < actors.length; i++) {
       const actor = actors[i]
       if (actor.getMapper().getInputData().getPointData().getGlobalIds() === upperTarget) {
-        actor.getProperty().setColor(1, 0, 0); // red
+        actor.getProperty().setColor(153/255, 0, 0); // penn red
         actor.getProperty().setOpacity(1);
       } else {
         actor.getProperty().setColor(0.5, 0.5, 0.5);
@@ -632,7 +633,7 @@ function App() {
                 e.preventDefault();
                 window.history.back();
               }}>&larr; Back</button>
-              <select className={"select select-bordered select-primary absolute top-0 " + (searched ? "left-24" : "left-0")} onChange={x => setSearchBy(x.target.value)}>
+              <select className={"select select-bordered select-primary rounded-r-none absolute top-0 " + (searched ? "left-24" : "left-0")} onChange={x => setSearchBy(x.target.value)}>
                 <option value="">Search by</option>
                 <option value="IDP">IDP</option>
                 <option value="SNP">SNP</option>
@@ -934,17 +935,17 @@ function App() {
           <span className="footer-title text-gray-500" style={{ opacity: 1 }}>Follow us</span>
           <div className="grid grid-flow-col gap-4">
             <a href="https://twitter.com/CBICAannounce" target="_blank" rel="noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current hover:text-secondary">
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
               </svg>
             </a>
             <a href="https://www.youtube.com/channel/UC69N7TN5bH2onj4dHcPLxxA" target="_blank" rel="noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current hover:text-secondary">
                 <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
               </svg>
             </a>
             <a href="https://www.facebook.com/CBICAAnnounce/" target="_blank" rel="noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current hover:text-secondary">
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
               </svg>
             </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,5 +12,36 @@ module.exports = {
   ],
   daisyui: {
     // themes: ['light']
+    themes: [
+      {
+        'penn': {                         /* theme name */
+          'primary': '#011F5B',           /* Primary color */
+          'primary-focus': '#990000',     /* Primary color - focused */
+          'primary-content': '#ffffff',   /* Foreground content color to use on primary color */
+
+          'secondary': '#990000',         /* Secondary color */
+          'secondary-focus': '#011F5B',   /* Secondary color - focused */
+          'secondary-content': '#ffffff', /* Foreground content color to use on secondary color */
+
+          'accent': '#570df8',            /* Accent color */
+          'accent-focus': '#4506cb',      /* Accent color - focused */
+          'accent-content': '#ffffff',    /* Foreground content color to use on accent color */
+
+          'neutral': '#3d4451',           /* Neutral color */
+          'neutral-focus': '#2a2e37',     /* Neutral color - focused */
+          'neutral-content': '#ffffff',   /* Foreground content color to use on neutral color */
+
+          'base-100': '#ffffff',          /* Base color of page, used for blank backgrounds */
+          'base-200': '#f9fafb',          /* Base color, a little darker */
+          'base-300': '#d1d5db',          /* Base color, even more darker */
+          'base-content': '#1f2937',      /* Foreground content color to use on base color */
+
+          'info': '#2094f3',              /* Info */
+          'success': '#009485',           /* Success */
+          'warning': '#ff9900',           /* Warning */
+          'error': '#ff5724',             /* Error */
+        },
+      },
+    ],
   }
 }
